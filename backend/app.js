8842db29510f9b171d482fb81e6ac7813f6aef42
Stephanie Carrero <stephanie_carrero@brown.edu>
2021-12-01 05:51:28 -0500

    connected node backend to mongodb database to store, add, and delete posts while refreshing frontend

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+
+const Post = require('./models/post')
 
 const app = express();
+
+
+// change this to personal mongo db database locally
+mongoose.connect("mongodb://localhost")
+.then(() => {
+  console.log('Connected to database!')
+})
+.catch(() => {
+  console.log('Connection failed!')
+})
 
 app.use(bodyParser.json());
 // app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,29 +28,35 @@ app.use((req,res,next) => {
 })
 
 app.post('/api/posts',(req,res,next) => {
-  const post = req.body;
-  console.log(post);
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: createdPost._id
+    });
+  });
   res.status(201).json({
     message: 'Post added successfully'
   });
 });
 
 app.get('/api/posts',(req,res,next) => {
-  const posts = [
-    {
-      id: "hfuierfhe432",
-      title:"server-side post",
-      content:"this is coming from the server"
-    },
-    {
-      id: "efeiufje",
-      title:"second server-side post",
-      content:"this is coming from the server again"
-    }
-  ]
-  res.status(200).json({
-    message: "post retrieved successfully",
-    posts: posts
+  Post.find().then(documents => {
+    console.log(documents);
+    res.status(200).json({
+      message: "post retrieved successfully",
+      posts: documents
+    });
+  });
+});
+
+app.delete("/api/posts/:id",(req,res,next) => {
+  Post.deleteOne({_id: req.params.id}).then(result => {
+    console.log(result);
+    res.status(200).json({message: "Post deleted"});
   });
 });
 
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -3,6 +3,7 @@ import { Post } from './post.model';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { stringify } from '@angular/compiler/src/util';
+import { map } from 'rxjs/operators'
 
 // provides this on the root level, can also add posts service to injectables in app module instead
 @Injectable({ providedIn: 'root' })
@@ -15,11 +16,20 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
+      .get<{ message: string; posts: any }>(
         'http://localhost:3000/api/posts'
       )
-      .subscribe(postData => {
-        this.posts = postData.posts;
+      .pipe(map((postData)=> {
+        return postData.posts.map(post =>  {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
       });
     // unsubscription to observable handled by angular
@@ -33,12 +43,25 @@ export class PostsService {
     const post: Post = { id: null, title: title, content: content };
     // updates posts on server
     this.http
-      .post<{ message: string }>('http://localhost:3000/api/posts', post)
+      .post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
       .subscribe(responseData => {
-        console.log(responseData.message);
+        const id = responseData.postId
+        post.id = id;
         // updates local posts
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
       });
   }
+
+  deletePost(postId: string) {
+    this.http.delete("http://localhost:3000/api/posts/" + postId)
+    .subscribe(() => {
+      const updatedPosts = this.posts.filter(post => post.id !== postId);
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+    });
+  }
+
 }
+
+
